eaf089e801529dbe97df656f6192e330d52099ee
jongwhan <leejw51@gmail.com>
2020-09-04 23:19:58 +0800

    package

--- sprite/app/app.go
+++ sprite/app/app.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	sprite "example.com/sprite/x/sprite"
+	fruit "example.com/sprite/x/sprite/fruit"
 )
 
 func main() {
 	fmt.Println("OK")
-	sprite.MyHelloWorld()
+	var a= sprite.MyHelloWorld()
+	fmt.Printf("%s\n", a)
+	fruit.FruitHelloWorld()
 }
--- sprite/x/sprite/module.go
+++ sprite/x/sprite/module.go
@@ -1,5 +1,5 @@
 package sprite 
 
 func MyHelloWorld() string {
-	return "test hello world"
+	return "sprite hello world"
 }
