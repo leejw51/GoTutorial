81e6c751cd060bb5846f3c8fa7a7a8d2b40a097d
jongwhan <leejw51@gmail.com>
2020-09-05 11:21:31 +0800

    reference ok

---
+++ sprite/x/sprite/food/food.go
@@ -0,0 +1,7 @@
+package food
+import "fmt"
+
+
+func FoodHelloWorld() {
+    fmt.Printf("food hello-world\n")
+}
--- sprite/x/sprite/fruit/apple.go
+++ sprite/x/sprite/fruit/apple.go
@@ -1,7 +1,11 @@
 package fruit
 
-import "fmt"
+import (
+	"fmt"
+	food "example.com/sprite/x/sprite/food"
+)
 
 func FruitHelloWorld() {
 	fmt.Println("fruit hello-world")
+	food.FoodHelloWorld()
 }
