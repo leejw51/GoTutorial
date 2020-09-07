e310970823338f018ea61886fe918fc26a529b6d
jongwhan <leejw51@gmail.com>
2020-09-07 17:30:31 +0800

    add map

--- sprite/engine/sample/map.go
+++ sprite/engine/sample/map.go
@@ -1,5 +1,13 @@
 package sample
 import "fmt"
 func TestMap() {
-	fmt.Println("test map")
+	m := make(map[string] int)
+	for i:=0;i<10;i++ {
+		var a=fmt.Sprintf("apple%d",i)
+		m[a]=i*100
+	}
+
+	for k,v  := range m {
+		fmt.Printf("key=%s value=%d\n", k,v)
+	}
 }
