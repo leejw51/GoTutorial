0859a947043a5e888746e9147a5cfc9f87e676f4
jongwhan <leejw51@gmail.com>
2020-09-07 17:16:25 +0800

    test map

--- sprite/app/app.go
+++ sprite/app/app.go
@@ -7,10 +7,14 @@ import (
 	sample "sprite/engine/sample"
 )
 
-func main() {
+func main2() {
 	fmt.Println("OK")
 	var a= engine.MyHelloWorld()
 	fmt.Printf("%s\n", a)
 	fruit.FruitHelloWorld()
 	sample.MySample2()
+}
+
+func main() {
+	sample.TestMap()
 }
---
+++ sprite/engine/sample/map.go
@@ -0,0 +1,5 @@
+package sample
+import "fmt"
+func TestMap() {
+	fmt.Println("test map")
+}
