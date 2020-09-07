a464aaa13f70335983cd39fe72e774674fbac2af
jongwhan <leejw51@gmail.com>
2020-09-07 21:45:19 +0800

    interface

--- sprite/engine/sample/interface.go
+++ sprite/engine/sample/interface.go
@@ -1,5 +1,24 @@
 package sample
 import "fmt"
+type Food interface {
+	price() int64
+}
+type Apple struct {
+	appleKind int64
+}
+func (a Apple) price() int64 {
+	return a.appleKind*1000
+}
+type Ramen struct {
+	ramenKind int64
+}
+func (r Ramen) price() int64 {
+	return r.ramenKind*20000
+}
 func TestInterface() {
 	fmt.Println("test interface")
+	a := Apple {appleKind:1}
+	r := Ramen {ramenKind:2}
+	fmt.Printf("apple %v\n", a)
+	fmt.Printf("ramen %v\n", r)
 }
