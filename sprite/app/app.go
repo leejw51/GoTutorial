35436a1535df4b317c14bcc77d153739c2a696cc
jongwhan <leejw51@gmail.com>
2020-09-11 11:09:26 +0800

    override

--- sprite/app/app.go
+++ sprite/app/app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"sprite/engine/sample"
 
 	"github.com/tendermint/tendermint/crypto/tmhash"
 )
@@ -13,10 +14,14 @@ func main2() {
 	fmt.Printf("hash = %s\n", hex.EncodeToString(a))
 }
 
-func main() {
+func main3() {
 	scores := [3]int{100, 200, 300}
 	scores2 := scores
 	scores[0] = 0
 	fmt.Printf("%v\n", scores)
 	fmt.Printf("%v\n", scores2)
+}
+
+func main() {
+	sample.TestOverride()
 }
---
+++ sprite/engine/sample/override.go
@@ -0,0 +1,38 @@
+package sample
+
+import "fmt"
+
+type Getter interface {
+	Get() string
+}
+
+type Drink struct {
+	Getter
+}
+
+func (base *Drink) Get() string {
+	return "drink"
+}
+
+func (base *Drink) GetName() string {
+	return base.Getter.Get()
+}
+
+//////////////////////////////////////////////////////////////////
+type AppleCider struct {
+	Drink
+}
+
+func (t *AppleCider) Get() string {
+	return "AppleCider"
+}
+func New() *AppleCider {
+	userType := &AppleCider{}
+	userType.Getter = interface{}(userType).(Getter)
+	return userType
+}
+
+func TestOverride() {
+	userType := New()
+	fmt.Println(userType.GetName()) // user string
+}
