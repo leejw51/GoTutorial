abe52f96edc4e2934f0789487c45c8e177eef00d
jongwhan <leejw51@gmail.com>
2020-09-11 10:39:15 +0800

    array test

--- sprite/app/app.go
+++ sprite/app/app.go
@@ -7,8 +7,16 @@ import (
 	"github.com/tendermint/tendermint/crypto/tmhash"
 )
 
-func main() {
+func main2() {
 	var b = []byte("helllo")
 	var a = tmhash.Sum(b)
 	fmt.Printf("hash = %s\n", hex.EncodeToString(a))
+}
+
+func main() {
+	scores := [3]int{100, 200, 300}
+	scores2 := scores
+	scores[0] = 0
+	fmt.Printf("%v\n", scores)
+	fmt.Printf("%v\n", scores2)
 }
