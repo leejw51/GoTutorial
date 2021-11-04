c77db2d0f8c4b924c76a46e654733c60987fa7ac
jongwhan <jonghwan@crypto.com>
2021-11-04 23:57:56 +0900

    compute

--- test_bigint/main.go
+++ test_bigint/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 func main() {
 	fmt.Println("hello world")
 
+	x1 := new(big.Int)
+	x1.SetString("100000000000000000000000000000000000000000", 10)
+	y1 := new(big.Int)
+	y1.SetString("3000000000000000000000000000000000000000000", 10)
+	x := big.NewFloat(0).SetInt(x1)
+	y := big.NewFloat(0).SetInt(y1)
+
+	z := new(big.Float).Quo(x, y)
+	z1 := new(big.Float).Quo(y, x)
+
+	fmt.Printf("z=%f\n", z)
+	fmt.Printf("z1=%f\n", z1)
+	fmt.Printf("-1 z1<z cmp=%d\n", z.Cmp(z1))
+	fmt.Printf("+1 z1>z cmp=%d\n", z1.Cmp(z))
 }
