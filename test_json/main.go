5343729fcbbd01453cf61307239ce929da5b95ea
jonghwan lee <leejw51@gmail.com>
2021-11-15 21:30:28 +0900

    numbers

--- test_json/main.go
+++ test_json/main.go
@@ -11,6 +11,8 @@ func main() {
 	data["price"] = 1000
 	data["apples"] = map[string]int{"apple": 5, "lettuce": 7}
 	data["fruits"] = []string{"strawberry", "mango", "pear"}
+	data["box"] = make([]int, 10, 100)
+	data["numbers"] = []int{10, 20, 30}
 	doc, _ := json.Marshal(data)
 	fmt.Println(string(doc))
 
