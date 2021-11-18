5618d6c3c18743813edc3a9cfbb098214d2625c4
jonghwan lee <leejw51@gmail.com>
2021-11-18 16:27:37 +0900

    json

--- test_json/main.go
+++ test_json/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func main() {
+func test1() {
 	data := make(map[string]interface{})
 	data["name"] = "apple"
 	data["price"] = 1000
@@ -15,5 +15,32 @@ func main() {
 	data["numbers"] = []int{10, 20, 30}
 	doc, _ := json.Marshal(data)
 	fmt.Println(string(doc))
+}
 
+func test2() {
+	data := make([]interface{}, 0)
+	data = append(data, 4)
+	data = append(data, "0x1c")
+	data = append(data, []int{25, 75})
+	doc, _ := json.Marshal(data)
+	fmt.Println(string(doc))
+
+}
+
+func main() {
+	data := make([]interface{}, 0)
+	data = append(data, 4)
+	data = append(data, "0x1c")
+	data = append(data, []int{25, 75})
+	doc, _ := json.Marshal(data)
+
+	var d struct {
+		a int    `json:"test"`
+		b string `json:"TEST"`
+		c []int  `json`
+	}
+	_ := json.Unmarshal(doc, &d)
+	data2, _ := json.Marshal([]int{25, 75})
+	fmt.Println(string(doc))
+	fmt.Println(string(data2))
 }
